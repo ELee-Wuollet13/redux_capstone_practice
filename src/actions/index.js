143531d817c6b2633da6b16ffca1158889d7a2e5
Student <example@example.com>
2020-03-06 17:00:57 -0800

    complete lesson up to API Requests with Async Actions and Redux-Thunk Middleware. Remeber to install Cors changer chrome add-on

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,11 +24,18 @@ export function fetchSongId(title) {
     const localSongId = v4();
     dispatch(requestSong(title, localSongId));
     title = title.replace(' ', '_');
-    return fetch('https://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=e1954d3468ed4493c255a62c40ddcc1f').then(
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=e1954d3468ed4493c255a62c40ddcc1f').then(
       response => response.json(),
       error => console.log('An error occurred.', error)
     ).then(function(json) {
-      console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+      if (json.message.body.track_list.length > 0) {
+        const musicMatchId = json.message.body.track_list[0].track.track_id;
+        const artist = json.message.body.track_list[0].track.artist_name;
+        const title = json.message.body.track_list[0].track.track_name;
+        fetchLyrics(title, artist, musicMatchId, localSongId, dispatch);
+      } else {
+        console.log('We couldn\'t locate a song under that ID!');
+      }
     });
   };
 }
--- src/reducers/lyricChangeReducer.js
+++ src/reducers/lyricChangeReducer.js
@@ -5,6 +5,7 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
   let newSongsByIdEntry;
   let newSongsByIdStateSlice;
   switch (action.type) {
+
     case types.NEXT_LYRIC:
     const newArrayPosition = state[action.currentSongId].arrayPosition + 1;
     newSongsByIdEntry = Object.assign({}, state[action.currentSongId], {
@@ -14,6 +15,7 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
       [action.currentSongId]: newSongsByIdEntry
     });
     return newSongsByIdStateSlice;
+
     case types.RESTART_SONG:
     newSongsByIdEntry = Object.assign({}, state[action.currentSongId], {
       arrayPosition: 0
@@ -22,6 +24,7 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
       [action.currentSongId]: newSongsByIdEntry
     });
     return newSongsByIdStateSlice;
+
     case types.REQUEST_SONG:
     newSongsByIdEntry = {
       isFetching: true,
@@ -32,6 +35,21 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
       [action.songId]: newSongsByIdEntry
     });
     return newSongsByIdStateSlice;
+
+    case types.RECEIVE_SONG:
+  newSongsByIdEntry = Object.assign({}, state[action.songId], {
+    isFetching: false,
+    receivedAt: action.receivedAt,
+    title: action.title,
+    artist: action.artist,
+    songArray: action.songArray,
+    arrayPosition: 0,
+    songId: action.songId
+  });
+  newSongsByIdStateSlice = Object.assign({}, state, {
+    [action.songId]: newSongsByIdEntry
+  });
+  return newSongsByIdStateSlice;
 
     default:
     return state;
